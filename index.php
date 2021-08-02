ce61ceae41bd7f582b14c7b79609e2a7a8ad8399
AlbertoAlfredo <raioalfredo@gmail.com>
2021-08-02 20:35:35 -0300

    primeiro commit

--- index.php
+++ index.php
@@ -167,7 +167,7 @@ if (defined('ENVIRONMENT'))
 	define('EXT', '.php');
 
 	// Path to the system folder
-	define('BASEPATH', str_replace("\\", "/", $system_path));
+	define('BASEPATH', str_replace("\\", "/mapos/", $system_path));
 
 	// Path to the front controller (this file)
 	define('FCPATH', str_replace(SELF, '', __FILE__));
